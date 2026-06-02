59d025f39a6de69e2ba9d36637bf27de318a5e6c
Teikey <mail.teikey@gmail.com>
2026-06-02 11:22:54 +0530

    initial

---
+++ script.js
@@ -0,0 +1,38 @@
+//DOM Connector
+const main = document.querySelector('.main-container');
+const clockDiv = document.createElement('div');
+clockDiv.classList.add('clock-container');
+main.appendChild(clockDiv);
+
+//Main
+function showTime() {
+    console.log('inside show Time function');
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }else{
+        session = "AM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+
+    clockDiv.innerText = time;
+    
+    setTimeout(showTime, 1000);
+}
+
+showTime();
